f925effdf94914b8a54342ac0db2b9d2bf211228
Oryza Sativa <oryzazar093@gmail.com>
2022-08-22 08:30:36 +0700

    ya gitudeh

---
+++ database/migrations/2022_08_18_021953_create_shipping_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping', function (Blueprint $table) {
+            $table->id();
+            $table->string('shipping');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping');
+    }
+}
