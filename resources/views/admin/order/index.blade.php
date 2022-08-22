e33727b5d128596680f63e3bbf87be8db6e91d93
hafitan n <mhmdanval@gmail.com>
2022-08-22 08:29:48 +0700

    lanjutan

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -23,6 +23,24 @@
                 <li class="breadcrumb-item active">Halaman Admin E-Commerce</li>
             </ol>
         </div>
+        <div class="container text-center">
+            <div class="row justify-content-start">
+                @foreach ($product as $p)
+                    <div class="col-mb-4">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img img-fluid" style="height: 200px ; width: 290px;" src="{{asset('public/image/'.$p->image)}}" alt="Card image cap" id="product-detail">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $p->name }}</h5>
+                                {{-- <p class="card-text">{!! $p->desc !!}</p> --}}
+                                <p>stock : {!! $p->stock !!}</p>
+                                <form action="{{ route('order.store') }}" method="post"></form>
+                                <a href="{{ route('order.store') }}" class="btn btn-primary"> beli</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 </div>
 @endsection
