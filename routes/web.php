f4aced97bf15b1f3b33069eb215f94323d38adad
hafitan n <mhmdanval@gmail.com>
2022-08-22 21:40:17 +0700

    oi

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,6 @@ Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 Auth::routes();
 
 Route::group(['prefix' => 'admin/', 'as' => 'admin.'], function () {
-<<<<<<< HEAD
-    
-=======
->>>>>>> 3b02b4a0773895934d8e7e9dad9892a91f1250db
 
     Route::group(['middleware' => 'auth','1'], function(){
         Route::resource('order', Admin\OrderController::class);
@@ -45,24 +41,6 @@ Route::group(['prefix' => 'admin/', 'as' => 'admin.'], function () {
         Route::resource('charts' , ChartsController::class);
         Route::post('restock' , 'Admin\ProductController@restock')->name('restock');
     });
-<<<<<<< HEAD
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    // route admin dashboard
-    Route::get('adminHome', [HomeController::class, 'adminHome'])->name('adminHome')->middleware('is_admin');
-});
-
-    Route::group(['middleware' => 'auth', 0], function(){
-        Route::post('chart' , 'ShopController@chart')->name('chart');
-        Route::post('keranjang' , 'ShopController@keranjang')->name('keranjang');
-        Route::post('checkout' , 'ShopController@bcheckout')->name('bcheckout');
-        Route::get('checkout/{id}' , 'ShopController@checkout')->name('checkout');
-        Route::post('buy' , 'ShopController@buy')->name('buy');
-    });
-
-    Route::get('single/{id}' , 'ShopController@single')->name('single');
-    Route::resource('shop' , ShopController::class);
- 
-=======
 
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -76,8 +54,6 @@ Route::group(['middleware' => 'auth', 0], function(){
     Route::post('checkout' , 'ShopController@bcheckout')->name('bcheckout');
     Route::get('checkout/{id}' , 'ShopController@checkout')->name('checkout');
 });
-
 Route::get('single/{id}' , 'ShopController@single')->name('single');
-Route::resource('shop' , ShopController::class);
 
->>>>>>> 3b02b4a0773895934d8e7e9dad9892a91f1250db
+Route::resource('shop' , ShopController::class);
