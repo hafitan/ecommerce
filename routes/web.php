1ee8c5e9a9e5b6a9c08e0c5585665028da4d5950
dimashervani <107090413+dimashervani@users.noreply.github.com>
2022-08-24 08:27:55 +0700

    pagination di category

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('product' , [App\Http\Controllers\admin\ProductController::class, 'index']);
-Route::get('category' , [App\Http\Controllers\admin\CategoryController::class, 'index']);
+Route::get('product' , [App\Http\Controllers\Asmin\ProductController::class, 'index']);
 
 
 Route::get('/', function () {
@@ -25,6 +24,7 @@ Route::get('/', function () {
 });
 Auth::routes();
 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 Auth::routes();
@@ -55,5 +55,19 @@ Route::group(['prefix' => 'admin/', 'as' => 'admin.'], function () {
         Route::get('checkout/{id}' , 'ShopController@checkout')->name('checkout');
         Route::post('buy' , 'ShopController@buy')->name('buy');
     });
+
     Route::get('single/{id}' , 'ShopController@single')->name('single');
     Route::resource('shop' , ShopController::class);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    // route admin dashboard
+    Route::get('adminHome', [HomeController::class, 'adminHome'])->name('adminHome')->middleware('is_admin');
+Route::group(['middleware' => 'auth', 0], function(){
+    Route::post('chart' , 'ShopController@chart')->name('chart');
+    Route::post('keranjang' , 'ShopController@keranjang')->name('keranjang');
+    Route::post('checkout' , 'ShopController@bcheckout')->name('bcheckout');
+    Route::get('checkout/{id}' , 'ShopController@checkout')->name('checkout');
+});
+
+Route::get('single/{id}' , 'ShopController@single')->name('single');
+
+Route::resource('shop' , ShopController::class);
